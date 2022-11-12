4e709d943c99d2478edbbf3e78c9144c9a0ae0ee
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-11-12 13:58:44 +0300

    fun getLocation added

--- app/src/main/java/com/rorono/weatherappavito/presentation/MainFragment.kt
+++ app/src/main/java/com/rorono/weatherappavito/presentation/MainFragment.kt
@@ -1,27 +1,44 @@
 package com.rorono.weatherappavito.presentation
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.activityViewModels
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.material.tabs.TabLayoutMediator
-import com.rorono.weatherappavito.R
 import com.rorono.weatherappavito.adapter.ViewPagerAdapter
 import com.rorono.weatherappavito.databinding.FragmentMainBinding
 import com.rorono.weatherappavito.utils.Constants
+import com.rorono.weatherappavito.utils.DialogManager
+import com.rorono.weatherappavito.utils.isPermissionGranted
+import com.rorono.weatherappavito.viewmodel.WeatherState
+import com.rorono.weatherappavito.viewmodel.WeatherViewModel
+import com.squareup.picasso.Picasso
 
 
 class MainFragment : Fragment() {
-    //private lateinit var locationClient: FusedLocationProviderClient
+    private lateinit var locationClient: FusedLocationProviderClient
     private lateinit var binding: FragmentMainBinding
+    private lateinit var pLauncher: ActivityResultLauncher<String>
     private val listFragments = listOf(TodayFragment.newInstance(),WeekFragment.newInstance())
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private val viewModel: WeatherViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,7 +50,75 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        checkPermission()
         initViewPagerAdapter()
+        updateCurrentWeather()
+        initLocationClient()
+        checkLocation()
+        binding.imUpdate.setOnClickListener {
+            getLocation()
+            binding.tabLayout.selectTab(binding.tabLayout.getTabAt(0))
+        }
+        binding.imSearch.setOnClickListener {
+            val result = binding.etSearchCityName.text.toString()
+            viewModel.getWeather(result)
+        }
+        viewModel.observeState(viewLifecycleOwner){
+            state->
+            when(state){
+
+                is WeatherState.Loading->{
+                    with(binding){
+                        progressBar.visibility = View.VISIBLE
+                        tvCurrentTemp.visibility = View.INVISIBLE
+                        tvData.visibility = View.INVISIBLE
+                        imWeather.visibility = View.INVISIBLE
+                        tvCityName.visibility = View.INVISIBLE
+                        tvCondition.visibility = View.INVISIBLE
+                        tvMinMaxTemp.visibility = View.INVISIBLE
+                        imSearch.visibility =View.INVISIBLE
+                        etSearchCityName.visibility = View.INVISIBLE
+                        tvData.visibility = View.INVISIBLE
+                    }
+                }
+                is WeatherState.Success ->{
+                    Log.d("TEST90","success")
+                       binding.progressBar.visibility = View.INVISIBLE
+                    with(binding){
+                        tvCurrentTemp.visibility = View.VISIBLE
+                        tvData.visibility = View.VISIBLE
+                        imWeather.visibility = View.VISIBLE
+                        tvCityName.visibility = View.VISIBLE
+                        tvCondition.visibility = View.VISIBLE
+                        tvMinMaxTemp.visibility = View.VISIBLE
+                        imSearch.visibility =View.VISIBLE
+                        etSearchCityName.visibility = View.VISIBLE
+                        tvData.visibility = View.VISIBLE
+                    }
+                }
+                is WeatherState.Error->{
+                    Log.d("TEST90","Error")
+                    with(binding){
+                        progressBar.visibility = View.INVISIBLE
+                        tvCurrentTemp.visibility = View.VISIBLE
+                        tvData.visibility = View.INVISIBLE
+                        imWeather.visibility = View.INVISIBLE
+                        tvCityName.visibility = View.INVISIBLE
+                        tvCondition.visibility = View.INVISIBLE
+                        tvMinMaxTemp.visibility = View.INVISIBLE
+                        imSearch.visibility =View.INVISIBLE
+                        etSearchCityName.visibility = View.INVISIBLE
+                        tvCondition.text = "Не удалось получить температуру по указанному городу. Пожалуйста проверьте правильность вводимых данных "
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkLocation()
+        getLocation()
     }
 
     private fun initViewPagerAdapter() = with(binding){
@@ -43,6 +128,76 @@ class MainFragment : Fragment() {
             tab.text = Constants.listNameTabLayout[position]
         }.attach()
     }
+
+    private fun  initLocationClient(){
+        locationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+    }
+    private fun permissionListener() {
+        pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun checkPermission() {
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+    private fun isLocationEnabled(): Boolean {
+        val locationManager =
+            activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+
+    private fun checkLocation(){
+        if (isLocationEnabled()){
+            getLocation()
+        }else{
+            DialogManager.getLocationDialog(requireContext(),object :DialogManager.GetLocationDialogListener{
+                override fun onClickPositiveButton() {
+                    startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                }
+
+            })
+        }
+    }
+    private fun getLocation() {
+        if (!isLocationEnabled()){
+            Toast.makeText(requireContext(),"Местоположение выключено",Toast.LENGTH_LONG).show()//переделать на tv
+            return
+        }
+        val ct = CancellationTokenSource()
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        locationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, ct.token)
+            .addOnCompleteListener {
+               viewModel.getWeather("${it.result.latitude},${it.result.longitude}")
+            }
+    }
+
+    private fun updateCurrentWeather()= with(binding){
+        viewModel.liveDataCurrentWeather.observe(viewLifecycleOwner){
+            Log.d("TEST100", "Я зашел сюда")
+            val maxMinTemp = "${it.maxTemp}C/${it.minTemp}C"
+            tvData.text = it.time
+            Picasso.get().load("https:" + it.imgUrl).into(imWeather)
+            tvCityName.text = it.city
+            tvCurrentTemp.text = it.currentTemp.ifEmpty {
+                "${it.maxTemp}/${it.minTemp}"
+            }
+            tvCondition.text = it.condition
+            tvMinMaxTemp.text = if (it.currentTemp.isEmpty()) "" else maxMinTemp
+        }
+    }
+
     companion object {
         @JvmStatic
         fun newInstance() =
