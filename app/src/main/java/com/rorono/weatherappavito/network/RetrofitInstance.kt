9cbf45cb4b46e371834ffbf74b29a6be22bfbe25
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-11-12 13:53:46 +0300

    fix

---
+++ app/src/main/java/com/rorono/weatherappavito/network/RetrofitInstance.kt
@@ -0,0 +1,33 @@
+package com.rorono.weatherappavito.network
+
+import com.rorono.weatherappavito.utils.Constants
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitInstance {
+    private val loggingInterceptor = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(120, TimeUnit.SECONDS)
+        .writeTimeout(120, TimeUnit.SECONDS)
+        .readTimeout(120, TimeUnit.SECONDS)
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    val api:WeatherApi by lazy {
+        retrofit.create(WeatherApi::class.java)
+    }
+
+}
