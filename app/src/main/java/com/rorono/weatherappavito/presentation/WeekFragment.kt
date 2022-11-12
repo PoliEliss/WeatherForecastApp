89607a216063515b79dcf7cc5872094c23cddc8f
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-11-12 13:54:55 +0300

    initRecyclerView added

--- app/src/main/java/com/rorono/weatherappavito/presentation/WeekFragment.kt
+++ app/src/main/java/com/rorono/weatherappavito/presentation/WeekFragment.kt
@@ -5,13 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rorono.weatherappavito.R
+import com.rorono.weatherappavito.adapter.ClickListener
+import com.rorono.weatherappavito.adapter.WeatherAdapter
 import com.rorono.weatherappavito.databinding.FragmentWeekBinding
+import com.rorono.weatherappavito.models.localmodels.WeatherModel
+import com.rorono.weatherappavito.viewmodel.WeatherViewModel
 
 
 class WeekFragment : Fragment() {
-
+    private lateinit var adapter: WeatherAdapter
     private lateinit var binding: FragmentWeekBinding
+    private val viewModel:WeatherViewModel by activityViewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,8 +33,26 @@ class WeekFragment : Fragment() {
         return binding.root
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+        viewModel.liveDataListWeather.observe(viewLifecycleOwner){
+            adapter.submitList(it)
+        }
+        adapter.setOnListener(object : ClickListener {
+            override fun onClick(weather: WeatherModel) {
+               viewModel.liveDataCurrentWeather.value = weather
+            }
+        })
+    }
 
+    private fun initRecyclerView() = with(binding){
+       recyclerViewWeek.layoutManager = LinearLayoutManager(requireContext())
+        adapter = WeatherAdapter()
+        recyclerViewWeek.adapter = adapter
+    }
+
+    companion object {
         @JvmStatic
         fun newInstance() = WeekFragment()
     }
