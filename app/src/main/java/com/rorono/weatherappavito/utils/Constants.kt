2240741c78603bbb137917262f0c9906a80c5ea9
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-11-12 13:57:15 +0300

    constants added

---
+++ app/src/main/java/com/rorono/weatherappavito/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.rorono.weatherappavito.utils
+
+object Constants {
+    const val BASE_URL ="https://api.weatherapi.com/v1/"
+    const val API_KEY ="aeb54558c27a42b89c293533220811"
+
+    val listNameTabLayout = listOf("TODAY", "WEEK")
+}
