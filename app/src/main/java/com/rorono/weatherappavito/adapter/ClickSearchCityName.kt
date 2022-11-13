fcd9eb6ab750eed6d8cdbb8e22d1ad1b8aa6a2d0
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-11-13 10:55:45 +0300

    ClickSearchCityName added

---
+++ app/src/main/java/com/rorono/weatherappavito/adapter/ClickSearchCityName.kt
@@ -0,0 +1,7 @@
+package com.rorono.weatherappavito.adapter
+
+import com.rorono.weatherappavito.models.remotemodels.SearchCityItem
+
+interface ClickSearchCityName {
+    fun onClick(searchCityItem: SearchCityItem)
+}
